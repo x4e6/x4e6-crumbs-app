b82d44f6f5b22df50c8bc66b9552ca7d874a72ec
Daria <daria@MacBook-Air-user.local>
2026-04-19 10:47:22 +0300

    Update CSV URL

--- src/app/config.js
+++ src/app/config.js
@@ -1,10 +1,10 @@
 export const config = {
   // If you publish a sheet as CSV, paste URL here.
   // Example:
-  // https://docs.google.com/spreadsheets/d/e/<...>/pub?gid=1705221888&single=true&output=csv
+  // https://docs.google.com/spreadsheets/d/e/2PACX-1vRQRtA3GgWRy9I021q5PP6jBLSES7XEQ4yEoR3oAOv6SNSnv5017PM1A8a7N4sfaA/pub?output=csv
   // Current source (provided by user):
   SHEET_CSV_URL:
-    "https://docs.google.com/spreadsheets/d/e/2PACX-1vTZbI7raXyPjMm4Zehw91ytEp2EKQrCTxfXzNX6mMxtxAoHzVE26Foev3CsXduZcw/pub?output=csv",
+    "https://docs.google.com/spreadsheets/d/e/2PACX-1vRQRtA3GgWRy9I021q5PP6jBLSES7XEQ4yEoR3oAOv6SNSnv5017PM1A8a7N4sfaA/pub?output=csv",
   // GID of the "Описание" tab in the same published spreadsheet (optional).
   // If set, the app will load intro cards from that tab.
   DESCRIPTION_GID: "",
